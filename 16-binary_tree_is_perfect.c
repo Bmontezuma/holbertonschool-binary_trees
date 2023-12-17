a704f844f5971d5b0d6c4a9f4a140f4f376d162b
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-17 05:13:17 -0800

    Is Perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,43 @@
 #include "binary_trees.h"
 
-/* Declaration of the calculate_depth function */
-int calculate_depth(const binary_tree_t *tree);
+/**
+ * binary_tree_height - Helper to calculate the height of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to calculate the height.
+ *
+ * Return: The height of the tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+ * binary_tree_is_full - Helper function to check if a binary tree is full.
+ *
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is full, 0 otherwise.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
+}
 
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect.
@@ -13,20 +49,11 @@ int calculate_depth(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1);
+		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+	size_t height = binary_tree_height(tree);
+	size_t size = binary_tree_size(tree);
 
-	if (tree->left != NULL && tree->right != NULL)
-	{
-		int left_depth = calculate_depth(tree->left);
-		int right_depth = calculate_depth(tree->right);
-
-		if (left_depth == right_depth)
-			return (binary_tree_is_perfect(tree->left) &&
-					binary_tree_is_perfect(tree->right));
-	}
-
-	return (0);
+	return (binary_tree_is_full(tree) && (size == ((size_t)1 << height) - 1));
 }
+
