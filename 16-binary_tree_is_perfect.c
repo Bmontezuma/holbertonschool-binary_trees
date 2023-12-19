ed1e481ce4eebf9131bc2d9451acc8b96903e930
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-12-18 19:55:54 -0800

    Is Perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,10 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
+
+/**
+ * calculate_depth - Calculate the depth of the tree
+ * @node: Pointer to the root of the tree
+ * Return: Depth of the tree
+ */
 int calculate_depth(const binary_tree_t *node)
 {
 	int depth = 0;
@@ -10,16 +14,18 @@ int calculate_depth(const binary_tree_t *node)
 		depth++;
 		node = node->left;
 	}
-
 	return (depth);
 }
 
+/**
+ * binary_tree_is_perfect - Check if a binary tree is perfect
+ * @tree: Pointer to the root of the tree
+ * Return: 1 if perfect, 0 otherwise
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-	{
 		return (0);
-	}
 
 	const binary_tree_t *queue[1024];
 	int front = 0, rear = 0;
@@ -34,9 +40,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		if (node->left == NULL && node->right == NULL)
 		{
 			if (front != depth)
-			{
 				return (0);
-			}
 		}
 		else if (node->left != NULL && node->right != NULL)
 		{
